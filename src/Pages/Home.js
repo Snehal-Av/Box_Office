780d0e47c2e2f4d70b8c9cc71a114334fbc9d556
Snehal-Av <snehalavatade31@gmail.com>
2023-05-05 20:46:25 +0530

    Fetch Abd Display api Data
    Fetch Abd Display api Data

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,25 +1,46 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import { SearchForShows } from '../api/TvMaze'
 
 const Home = () => {
-  const[searchInput,setSearchInput]=useState('')
-  const SearchHandler=(e)=>{
+  const [searchInput, setSearchInput] = useState('')
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null)
+  const SearchHandler = (e) => {
     setSearchInput(e.target.value)
   }
 
-  const OnSearch=async(e)=>{
+  const OnSearch = async (e) => {
     e.preventDefault()
-    const response=await fetch(`https://api.tvmaze.com/search/shows?q=${searchInput}`)
-    const body=await response.json()
-    console.log(body)
+    try {
+      setApiDataError(null)
+      const result = await SearchForShows(searchInput)
+      setApiData(result)
+    } catch (error) {
+      setApiDataError(error)
+    }
+  }
+
+  const RenderApiData = () => {
+
+    if (apiDataError) {
+      return <div>Error Occurred:{apiDataError.message}</div>
+    }
+
+    if (apiData) {
+      return apiData.map((data) =>
+        <div key={data.show.id}>{data.show.name}</div>
+      )
+    }
   }
 
   return (
     <div>
       <form onSubmit={OnSearch}>
-        <input type='text' value={searchInput} onChange={SearchHandler}/>
+        <input type='text' value={searchInput} onChange={SearchHandler} />
         <button type='submit'>search</button>
       </form>
+      <div>{RenderApiData()}</div>
     </div>
   )
 }
---
+++ src/api/TvMaze.js
@@ -0,0 +1,8 @@
+const BASE_URL="https://api.tvmaze.com"
+const getApi=async(QueryString)=>{
+    const response=await fetch(`${BASE_URL}${QueryString}`)
+    const body=await response.json()
+    return body
+}
+
+export const SearchForShows=(query)=>(getApi(`/search/shows?q=${query}`))
