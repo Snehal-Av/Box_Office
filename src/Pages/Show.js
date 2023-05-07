678708b95ea73ca451a3ec0e3e5baf7f4acb2ab8
Snehal-Av <snehalavatade31@gmail.com>
2023-05-07 16:54:36 +0530

    Get ApiData inside Show Component
    Get ApiData inside Show Component

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { GetShowById } from '../api/TvMaze'
 
 const Show = () => {
-    const {showId}=useParams()
+  const { showId } = useParams()
+
+  const[showData,setShowData]=useState(null);
+  const[showError,setShowError]=useState(null);
+
+  useEffect(() => {
+    const fetchData=async()=>{
+    try {
+      const data=await GetShowById(showId)
+      setShowData(data)
+    } catch (error) {
+      setShowError(error)
+    }
+    }
+    fetchData()
+  },[showId]);
+
+  if(showError){
+    return<div>we have Error:{showError.message}</div>
+  }
+
+  if(showData){
+    return <div>We have DAta:{showData.name}</div>
+  }
+  
   return (
     <div>
-        <h1>Show Page for {showId}</h1>
+    <div>Data is loading</div>
     </div>
   )
 }
